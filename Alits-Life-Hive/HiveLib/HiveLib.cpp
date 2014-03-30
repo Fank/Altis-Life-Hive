de249f50a1bcaccbf0e785b64165ca23aa77d179
Florian Kinder <Florian.Kinder@fankserver.com>
2014-03-30 03:08:53 +0200

    Final player update

--- Alits-Life-Hive/HiveLib/HiveLib.cpp
+++ Alits-Life-Hive/HiveLib/HiveLib.cpp
@@ -98,7 +98,7 @@ void HiveLib::setPlayerCop(__int64 _steamId, int _cash, int _bank, const char *_
 	sqlQuery << "cop_licenses = ? ";
 	sqlQuery << "WHERE playerid = " << _steamId;
 	if (this->debugLogQuery) {
-		this->log(sqlQuery.str().c_str());
+		this->log(sqlQuery.str().c_str(), __FUNCTION__);
 	}
 
 	sqlStatement = mysql_stmt_init(this->MySQLStack[HIVELIB_MYSQL_CONNECTION_PLAYERUPDATE]);
@@ -106,7 +106,7 @@ void HiveLib::setPlayerCop(__int64 _steamId, int _cash, int _bank, const char *_
 		if (mysql_stmt_prepare(sqlStatement, sqlQuery.str().c_str(), sqlQuery.str().size()) == 0) {
 			memset(sqlParam, 0, sizeof(sqlParam));
 
-			// player name
+			// bind player name
 			long unsigned int playerNameLength;
 			sqlParam[0].buffer_type = MYSQL_TYPE_STRING;
 			sqlParam[0].buffer_length = 32;
@@ -114,21 +114,33 @@ void HiveLib::setPlayerCop(__int64 _steamId, int _cash, int _bank, const char *_
 			sqlParam[0].is_null = 0;
 			sqlParam[0].length = &playerNameLength;
 
+			// bind gear
 			long unsigned int gearLength;
 			sqlParam[1].buffer_type = MYSQL_TYPE_STRING;
-			sqlParam[1].buffer_length = 4096;
+			sqlParam[1].buffer_length = 2048;
 			sqlParam[1].buffer = (char *)_gear;
 			sqlParam[1].is_null = 0;
 			sqlParam[1].length = &gearLength;
 
+			// bind licenses
 			long unsigned int licensesLength;
 			sqlParam[2].buffer_type = MYSQL_TYPE_STRING;
-			sqlParam[2].buffer_length = 4096;
+			sqlParam[2].buffer_length = 2048;
 			sqlParam[2].buffer = (char *)_licenses;
 			sqlParam[2].is_null = 0;
 			sqlParam[2].length = &licensesLength;
 
-			if (!mysql_stmt_bind_param(sqlStatement, sqlParam)) {
+			// bind to statement
+			if (mysql_stmt_bind_param(sqlStatement, sqlParam)) {
+				std::stringstream errorMsg;
+				errorMsg << "mysql_stmt_bind_param() failed: " << mysql_stmt_error(sqlStatement);
+				this->log(errorMsg.str().c_str(), __FUNCTION__);
+			}
+			else {
+				playerNameLength = strlen(_playerName);
+				gearLength = strlen(_gear);
+				licensesLength = strlen(_licenses);
+
 				// Request meta data information
 				MYSQL_RES *sqlResult = mysql_stmt_result_metadata(sqlStatement);
 
@@ -136,19 +148,14 @@ void HiveLib::setPlayerCop(__int64 _steamId, int _cash, int _bank, const char *_
 				my_bool aBool = 1;
 				mysql_stmt_attr_set(sqlStatement, STMT_ATTR_UPDATE_MAX_LENGTH, &aBool);
 
-				if (!mysql_stmt_execute(sqlStatement)) {
-
-				}
-				else {
+				if (mysql_stmt_execute(sqlStatement)) {
 					std::stringstream errorMsg;
 					errorMsg << "mysql_stmt_execute(), 1 failed\n" << mysql_stmt_error(sqlStatement);
 					this->log(errorMsg.str().c_str(), __FUNCTION__);
 				}
-			}
-			else {
-				std::stringstream errorMsg;
-				errorMsg << "mysql_stmt_bind_param() failed: " << mysql_stmt_error(sqlStatement);
-				this->log(errorMsg.str().c_str(), __FUNCTION__);
+				else {
+					// success :)
+				}
 			}
 		}
 		else {
@@ -160,10 +167,168 @@ void HiveLib::setPlayerCop(__int64 _steamId, int _cash, int _bank, const char *_
 	}
 }
 void HiveLib::setPlayerCiv(__int64 _steamId, int _cash, int _bank, const char *_gear, const char *_licenses, bool _arrested, const char *_playerName) {
+	MYSQL_STMT *sqlStatement;
+	MYSQL_BIND sqlParam[3];
 
+	std::stringstream sqlQuery;
+	sqlQuery << "UPDATE players SET ";
+	sqlQuery << "name = ?, ";
+	sqlQuery << "cash = " << _cash << ", ";
+	sqlQuery << "bankacc = " << _bank << ", ";
+	sqlQuery << "civ_gear = ?, ";
+	sqlQuery << "civ_licenses = ?, ";
+	sqlQuery << "arrested = " << (_arrested ? 1 : 0) << " ";
+	sqlQuery << "WHERE playerid = " << _steamId;
+	if (this->debugLogQuery) {
+		this->log(sqlQuery.str().c_str(), __FUNCTION__);
+	}
+
+	sqlStatement = mysql_stmt_init(this->MySQLStack[HIVELIB_MYSQL_CONNECTION_PLAYERUPDATE]);
+	if (sqlStatement != NULL) {
+		if (mysql_stmt_prepare(sqlStatement, sqlQuery.str().c_str(), sqlQuery.str().size()) == 0) {
+			memset(sqlParam, 0, sizeof(sqlParam));
+
+			// bind player name
+			long unsigned int playerNameLength;
+			sqlParam[0].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[0].buffer_length = 32;
+			sqlParam[0].buffer = (char *)_playerName;
+			sqlParam[0].is_null = 0;
+			sqlParam[0].length = &playerNameLength;
+
+			// bind gear
+			long unsigned int gearLength;
+			sqlParam[1].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[1].buffer_length = 2048;
+			sqlParam[1].buffer = (char *)_gear;
+			sqlParam[1].is_null = 0;
+			sqlParam[1].length = &gearLength;
+
+			// bind licenses
+			long unsigned int licensesLength;
+			sqlParam[2].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[2].buffer_length = 2048;
+			sqlParam[2].buffer = (char *)_licenses;
+			sqlParam[2].is_null = 0;
+			sqlParam[2].length = &licensesLength;
+
+			// bind to statement
+			if (mysql_stmt_bind_param(sqlStatement, sqlParam)) {
+				std::stringstream errorMsg;
+				errorMsg << "mysql_stmt_bind_param() failed: " << mysql_stmt_error(sqlStatement);
+				this->log(errorMsg.str().c_str(), __FUNCTION__);
+			}
+			else {
+				playerNameLength = strlen(_playerName);
+				gearLength = strlen(_gear);
+				licensesLength = strlen(_licenses);
+
+				// Request meta data information
+				MYSQL_RES *sqlResult = mysql_stmt_result_metadata(sqlStatement);
+
+				// Set STMT_ATTR_UPDATE_MAX_LENGTH attribute
+				my_bool aBool = 1;
+				mysql_stmt_attr_set(sqlStatement, STMT_ATTR_UPDATE_MAX_LENGTH, &aBool);
+
+				if (mysql_stmt_execute(sqlStatement)) {
+					std::stringstream errorMsg;
+					errorMsg << "mysql_stmt_execute(), 1 failed\n" << mysql_stmt_error(sqlStatement);
+					this->log(errorMsg.str().c_str(), __FUNCTION__);
+				}
+				else {
+					// success :)
+				}
+			}
+		}
+		else {
+			this->log("Could not prepare statement", __FUNCTION__);
+		}
+	}
+	else {
+		this->log("Could not initialize statement handler", __FUNCTION__);
+	}
 }
 void HiveLib::setPlayerReb(__int64 _steamId, int _cash, int _bank, const char *_gear, const char *_licenses, bool _arrested, const char *_playerName) {
+	MYSQL_STMT *sqlStatement;
+	MYSQL_BIND sqlParam[3];
 
+	std::stringstream sqlQuery;
+	sqlQuery << "UPDATE players SET ";
+	sqlQuery << "name = ?, ";
+	sqlQuery << "cash = " << _cash << ", ";
+	sqlQuery << "bankacc = " << _bank << ", ";
+	sqlQuery << "reb_gear = ?, ";
+	sqlQuery << "civ_licenses = ? ";
+	sqlQuery << "arrested = " << (_arrested ? 1 : 0) << " ";
+	sqlQuery << "WHERE playerid = " << _steamId;
+	if (this->debugLogQuery) {
+		this->log(sqlQuery.str().c_str(), __FUNCTION__);
+	}
+
+	sqlStatement = mysql_stmt_init(this->MySQLStack[HIVELIB_MYSQL_CONNECTION_PLAYERUPDATE]);
+	if (sqlStatement != NULL) {
+		if (mysql_stmt_prepare(sqlStatement, sqlQuery.str().c_str(), sqlQuery.str().size()) == 0) {
+			memset(sqlParam, 0, sizeof(sqlParam));
+
+			// bind player name
+			long unsigned int playerNameLength;
+			sqlParam[0].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[0].buffer_length = 32;
+			sqlParam[0].buffer = (char *)_playerName;
+			sqlParam[0].is_null = 0;
+			sqlParam[0].length = &playerNameLength;
+
+			// bind gear
+			long unsigned int gearLength;
+			sqlParam[1].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[1].buffer_length = 2048;
+			sqlParam[1].buffer = (char *)_gear;
+			sqlParam[1].is_null = 0;
+			sqlParam[1].length = &gearLength;
+
+			// bind licenses
+			long unsigned int licensesLength;
+			sqlParam[2].buffer_type = MYSQL_TYPE_STRING;
+			sqlParam[2].buffer_length = 2048;
+			sqlParam[2].buffer = (char *)_licenses;
+			sqlParam[2].is_null = 0;
+			sqlParam[2].length = &licensesLength;
+
+			// bind to statement
+			if (mysql_stmt_bind_param(sqlStatement, sqlParam)) {
+				std::stringstream errorMsg;
+				errorMsg << "mysql_stmt_bind_param() failed: " << mysql_stmt_error(sqlStatement);
+				this->log(errorMsg.str().c_str(), __FUNCTION__);
+			}
+			else {
+				playerNameLength = strlen(_playerName);
+				gearLength = strlen(_gear);
+				licensesLength = strlen(_licenses);
+
+				// Request meta data information
+				MYSQL_RES *sqlResult = mysql_stmt_result_metadata(sqlStatement);
+
+				// Set STMT_ATTR_UPDATE_MAX_LENGTH attribute
+				my_bool aBool = 1;
+				mysql_stmt_attr_set(sqlStatement, STMT_ATTR_UPDATE_MAX_LENGTH, &aBool);
+
+				if (mysql_stmt_execute(sqlStatement)) {
+					std::stringstream errorMsg;
+					errorMsg << "mysql_stmt_execute(), 1 failed\n" << mysql_stmt_error(sqlStatement);
+					this->log(errorMsg.str().c_str(), __FUNCTION__);
+				}
+				else {
+					// success :)
+				}
+			}
+		}
+		else {
+			this->log("Could not prepare statement", __FUNCTION__);
+		}
+	}
+	else {
+		this->log("Could not initialize statement handler", __FUNCTION__);
+	}
 }
 
 void HiveLib::log(const char *_logMessage) {
