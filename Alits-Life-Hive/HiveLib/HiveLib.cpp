ea5896817d741e19dcbbc91fa47e2edf498053f8
Florian Kinder <Florian.Kinder@mcl.de>
2014-04-08 13:29:13 +0200

    refs #2

--- Alits-Life-Hive/HiveLib/HiveLib.cpp
+++ Alits-Life-Hive/HiveLib/HiveLib.cpp
@@ -4,8 +4,8 @@
 
 HiveLib::HiveLib(char *_profilePath) {
 	// Disable debug
-	this->debugLogQuery = true;
-	this->debugLogResult = true;
+	this->debugLogQuery = false;
+	this->debugLogResult = false;
 
 	// Set profile path
 	const char *configFile = "AltisLifeHive.cfg";
